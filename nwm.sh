c79f5cc748127add042b60f3a1225e2eef05a597
Nick Niemeir <nick.niemeir@gmail.com>
2012-02-22 07:42:37 -0700

    making xmodmap work from cwd

--- nwm.sh
+++ nwm.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 xset b off
-xmodmap ~/.xmodmap
+xmodmap ./xmodmap
 exec /usr/local/bin/coffee /home/nick/github/nwm-user/nwm-user.coffee 2>/home/nick/github/nwm-user/nwm.err.log 1>/home/nick/github/nwm-user/nwm.log
